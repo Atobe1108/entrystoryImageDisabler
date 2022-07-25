a74987c6a185df3df7bc1807ddd257b68162e4b3
Atobe <93640494+Atobe1108@users.noreply.github.com>
2022-07-25 20:32:23 +0900

    Create popup.js

---
+++ Dist/popup.js
@@ -0,0 +1,7 @@
+function a () {
+    if(location.href.indexOf('https://playentry.org/community/entrystory') != -1) {
+        console.log("완료.");
+        Array.from(document.getElementsByClassName("css-18bdrlk enx4swp0")).forEach(a=>{a.style.cssText=null;})
+    }}
+   
+    setInterval(a, 500);
